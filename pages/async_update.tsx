c350d358ce7a14a896bfe8e394dec7e62257b2b5
Yash Mahalwal <yashmahalwal@gmail.com>
2019-12-21 10:33:06 +0530

    feat(async update): added the labels for logs

--- pages/async_update.tsx
+++ pages/async_update.tsx
@@ -2,72 +2,74 @@ import React from "react";
 
 interface Props {}
 interface State {
-  value: number;
+    value: number;
 }
 
 class AsyncBatchIncorrect extends React.Component<Props, State> {
-  static displayName: string;
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      value: 0
-    };
-  }
+    static displayName: string;
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            value: 0
+        };
+    }
 
-  componentDidMount() {
-    this.setState({ value: 10 });
-    console.log("State is set to 10");
-    this.setState({ value: this.state.value + 1 });
-    console.log(this.state.value);
-  }
+    componentDidMount() {
+        console.log("Incorrect");
+        this.setState({ value: 10 });
+        console.log("State is set to 10");
+        this.setState({ value: this.state.value + 1 });
+        console.log(this.state.value);
+    }
 
-  render() {
-    return (
-      <>
-        <p>Incorrect:</p>
-        <h1>State: {this.state.value}</h1>
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <p>Incorrect:</p>
+                <h1>State: {this.state.value}</h1>
+            </>
+        );
+    }
 }
 
 class AsyncBatchCorrect extends React.Component<Props, State> {
-  static displayName: string;
-  constructor(props: Props) {
-    super(props);
-    this.state = {
-      value: 0
-    };
-  }
+    static displayName: string;
+    constructor(props: Props) {
+        super(props);
+        this.state = {
+            value: 0
+        };
+    }
 
-  componentDidMount() {
-    this.setState({ value: 10 }, () => {
-      console.log("State is set to 10");
-      this.setState(
-        ({ value }) => ({
-          value: value + 1
-        }),
-        () => console.log(this.state.value)
-      );
-    });
-  }
+    componentDidMount() {
+        console.log("Correct");
+        this.setState({ value: 10 }, () => {
+            console.log("State is set to 10");
+            this.setState(
+                ({ value }) => ({
+                    value: value + 1
+                }),
+                () => console.log(this.state.value)
+            );
+        });
+    }
 
-  render() {
-    return (
-      <>
-        <p>Correct:</p>
-        <h1>State: {this.state.value}</h1>
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <p>Correct:</p>
+                <h1>State: {this.state.value}</h1>
+            </>
+        );
+    }
 }
 
 const AsyncBatch: React.FunctionComponent = () => (
-  <>
-    <AsyncBatchIncorrect />
-    <hr />
-    <AsyncBatchCorrect />
-  </>
+    <>
+        <AsyncBatchIncorrect />
+        <hr />
+        <AsyncBatchCorrect />
+    </>
 );
 
 AsyncBatch.displayName = "AsynchronousStateUpdate";
